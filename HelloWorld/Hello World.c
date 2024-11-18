8a3a624dbdc92669557d2c40b68ce91121fd32fb
Raju VIshawakarma <rajuvish547@gmail.com>
2024-11-18 23:08:25 +0530

    Add files via upload

---
+++ HelloWorld/Hello World.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+/**
+ * This code is written in C programming language.
+ * It contains a main function that prints "hello world" to the console.
+ */
+    void main(){
+        printf("hello world");
+    }
