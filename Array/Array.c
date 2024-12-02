47119ec281de84026f0559c0cfe4476c8c1dbe66
Raju VIshawakarma <rajuvish547@gmail.com>
2024-12-02 07:23:29 +0000

    SIMPLE ARRAY CODE

---
+++ Array/Array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+    void main(){
+        int i, array[]={1,2,3,4,5,6,7,8,9,0};
+        for(i=0; i<10; i++)
+        {
+            printf("%d\n",array[i]);
+           
+            
+        
+           
+        }  
+    }
+    // OUTPUT
+/*1
+2
+3
+4
+5
+6
+7
+8
+9
+0
+*/
